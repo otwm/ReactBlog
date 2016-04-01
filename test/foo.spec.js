9f029fa6739d886c988e53b2016b952153654f5d
kdo <otwmoo@gmail.com>
2016-04-01 11:30:26 +0900

    init commit

---
+++ test/foo.spec.js
@@ -0,0 +1,10 @@
+import expect from 'expect'
+import React from 'react'
+// import {shallow} from 'enzyme'
+// import Counter from '../../components/Counter'
+
+describe('foo', () => {
+    it('nothing', () => {
+        
+    })
+})
